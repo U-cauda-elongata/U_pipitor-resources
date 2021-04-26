U-cauda-elongata/U_pipitor-resources

commit 33fb2145e50534a5a87b0d50d276714c8872e6fa

今度こそアニメ！アニメ！　のアンケート記事を完全にやっつける

Pipitor.dhall
@@ -404,7 +404,7 @@ let rule = [
   },
   Rule::{
     filter = Some Filter::{ title = basicFilter},
-    exclude = Some Filter::{ title = basicExclude ++ "|🏆|アンケート〆切" }, -- アンケート記事
+    exclude = Some Filter::{ title = basicExclude ++ "🏆|アンケート" }, -- アンケート記事
     outbox = [Outbox.Twitter pipitor],
     topics = [
       Twitter 456544724, -- @AnimeAnime_jp アニメ！アニメ！

commit 28e0f5c62db8dc0a1d9e830d92e5a62b0227fac2

正規表現の文法エラーの修正
Regressed in 33fb2145e50534a5a87b0d50d276714c8872e6fa.

Pipitor.dhall
@@ -414,7 +414,7 @@ let rule = [
   },
   Rule::{
     filter = Some Filter::{ title = basicFilter},
-    exclude = Some Filter::{ title = basicExclude ++ "🏆|アンケート" }, -- アンケート記事
+    exclude = Some Filter::{ title = basicExclude ++ "|🏆|アンケート" }, -- アンケート記事
     outbox = [Outbox.Twitter pipitor],
     topics = [
       Twitter 456544724, -- @AnimeAnime_jp アニメ！アニメ！